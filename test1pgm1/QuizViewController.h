0232df2b14965204295a9c10c5f9875446813f05
K-RKidd <kr248196@muhlenberg.edu>
2014-01-25 21:32:54 -0500

    Initial Commit

---
+++ test1pgm1/QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  test1pgm1
+//
+//  Created by Krystle on 1/25/14.
+//  Copyright (c) 2014 Krystle Kidd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
